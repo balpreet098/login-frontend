83a8772c14d659fb736e479b7b73a0077165245c
balpreet098 <balpreet.immanent@gmail.com>
2024-07-04 18:01:18 +0530

    updated four

--- src/ForgetPassword/ForgetPassword.jsx
+++ src/ForgetPassword/ForgetPassword.jsx
@@ -9,7 +9,7 @@ export default function ForgetPassword() {
   const navigate = useNavigate();
 
   const validateEmail = () => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
     if (!email.match(emailRegex)) {
       setErrorMessage("Invalid email format");
       return false;
